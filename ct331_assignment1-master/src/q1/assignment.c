798e48238cc7c60bd3f1b9fa621fb4605d7e4bb0
Ihor Debelyi <klerk333@gmail.com>
2017-09-28 14:27:43 +0100

    Error size_t

---
+++ ct331_assignment1-master/src/q1/assignment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int arg, char* argc[]){
+  int a;
+  int* b;
+  long long int c;
+  double * d;
+  char ** f;
+  printf("%d\n", sizeof(a) );
+  printf("%d\n", sizeof(b) );
+  printf("%d\n", sizeof(c) );
+  printf("%d\n", sizeof(d) );
+  printf("%d\n", sizeof(f) );
+  printf("%d\n", sizeof(r) );
+  return 0;
+}
---
+++ ct331_assignment1-master/src/q2/tests.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "tests.h"
+#include "linkedList.h"
+
+void runTests(){
+  printf("Tests running...\n");
+  listElement* l = createEl("Test String (1).", 30);
+  //printf("%s\n%p\n", l->data, l->next);
+  //Test create and traverse
+  traverse(l);
+  printf("\n");
+
+  //Test insert after
+  listElement* l2 = insertAfter(l, "another string (2)", 30);
+  insertAfter(l2, "a final string (3)", 30);
+  traverse(l);
+  printf("\n");
+
+  // Test delete after
+  deleteAfter(l);
+  traverse(l);
+  printf("\n");
+
+  //Test length
+  int num = length(l2);
+  printf("%d\n", num);
+
+  //Test push
+  push(l2, "a final last string (4)", 30);
+  traverse(l2);
+  printf("\nTests complete.\n");
+}
